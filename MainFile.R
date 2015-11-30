e78259017b9dbdc2ffec55038b6b43d631566d0e
fschulz <sulzfran@hu-berlin.de>
2015-11-30 14:59:33 +0100

    Create MainFile.R

---
+++ MainFile.R
@@ -0,0 +1,75 @@
+# ------------------------------------------------------------------------------
+# Paper: Volatility linkages between energy and agricultural commodity prices
+# ------------------------------------------------------------------------------
+# Quantlet: - 
+# ------------------------------------------------------------------------------
+# Description: Code used to make the plots and tables presented in the paper
+# ------------------------------------------------------------------------------
+# Usage: -
+# ------------------------------------------------------------------------------
+# Inputs: Data.R, FGLS.R, RiskManagement.R
+# ------------------------------------------------------------------------------
+# Output: -
+# ------------------------------------------------------------------------------
+# Keywords: Energy, Agriculture, Commodities
+# ------------------------------------------------------------------------------
+# See also: -
+# ------------------------------------------------------------------------------
+# Author: Brenda Lòpez Cabrera, Franziska Schulz
+# ------------------------------------------------------------------------------
+
+# Clear memory
+rm(list = ls())
+# Set working directory
+setwd("C:\\Users\\Franziska Schulz\\Code")
+
+# install and load libraries
+libraries = c("car",
+              "abind",
+              "quantmod",
+              "zoo", 
+              "Epi",
+              "tseries",
+              "urca",
+              "vars",
+              "tsDyn",
+              "stats", 
+              "fGarch",
+              "np",
+              "corpcor",
+              "FinTS",
+              "rugarch", 
+              "rmgarch",
+              "Rcpp",
+              "truncnorm",
+              "Kendall",
+              "uroot", 
+              "sm",
+              "ccgarch")
+
+lapply(libraries, function(x) if (!(x %in% installed.packages())) {
+    install.packages(x)
+})
+
+getwd()
+
+source("Data.R")
+source("Plot1.R")
+source("Table2.R")
+source("Table3.R")
+source("Table4.R")
+source("VECM.R")  # Equation 17 + 18 /
+source("Plot3.R")
+source("Table5.R") /
+source("Plot4&5.R")
+source("FGLS.R")  # Table 7 + 8
+source("Table6.R")
+source("Plot2.R")
+source("Plot6.R")
+source("RF_Plot7.R")
+source("Plot8.R") /
+source("Plot9.R")
+source("Table9.R")
+source("Table10.R")
+# Application to Hedging:
+source("RiskManagement.R")
